26d339f50210e6ab5edd9d708a58a9ebfedabf35
ShivangiJaiswal026 <your_github_email@example.com>
2025-12-11 04:07:44 +0530

    added readme

--- app/src/main/java/com/newsdash/util/AppConstants.kt
+++ app/src/main/java/com/newsdash/util/AppConstants.kt
@@ -3,7 +3,7 @@ package com.newsdash.util
 class AppConstants {
     companion object {
         const val BASE_URL = "https://gnews.io/api/v4/"
-        const val API_KEY = "885f93ea427dd67dfeab1ce5bd0e09c9"
+        const val API_KEY = "DUMMY_API_KEY"
         const val GENERAL = "general"
         const val EN = "en"
         const val IN = "in"
